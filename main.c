174b4f32328fc9b0cabfb67da979cc3c5ae6178c
3riple6ix <shuherlolol@gmail.com>
2018-05-14 22:34:30 +0300

    added commit

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
+// programma slojeniya i vichitaniya
 #include <stdio.h>
 int main ()
 {
 int x,y;
  //printf("enter to integer\n");
 scanf("%d\n%d", &x, &y);
-printf("SUM IS - %d\n", x, y);
+printf("SUM IS - %d\n diff is - %d ", x + y, x - y);
 return 0;
 }
