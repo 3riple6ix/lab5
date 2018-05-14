b1e07f997233eb200a749b212bcd2eece665093c
3riple6ix <shuherlolol@gmail.com>
2018-05-14 21:59:18 +0300

    main.c modified again

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 int main ()
 {
 int x,y;
-printf("enter to integer\n");
+ //printf("enter to integer\n");
 scanf("%d\n%d", &x, &y);
-printf("sum=%d\n", x, y);
+printf("SUM IS - %d\n", x, y);
 return 0;
 }
